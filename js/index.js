7bcca358f112d8bbfd39ad7e579d954909651103
Eduardo Cabrera <lalo_cabrera1977@hotmail.com>
2022-05-03 23:15:12 -0500

    Version final

--- js/index.js
+++ js/index.js
@@ -1,85 +1,11 @@
+import Model from './model.js';
+import View from './view.js';
 
-document.addEventListener('DOMContentLoaded', function (){
-	const title = document.getElementById('title');
-	const description = document.getElementById('description');
-	const table = document.getElementById('table');
-	const alert = document.getElementById('alert');
-	const btn = document.getElementById('add');
-   let id=1;
+document.addEventListener('DOMContentLoaded', () => {
+  const model = new Model();
+  const view = new View();
+  model.setView(view);
+  view.setModel(model);
 
-   function removeTodo(id){
-	   console.log(id);
-	   document.getElementById(id).remove();
-   }
-
-	function addTodo(){
-		if(title.value === '' || description.value === ''){
-			alert.classList.remove('d-none');
-			alert.innerText= 'Titulo y descripcion requeridos';
-			return;
-		}
-	   alert.classList.add('d-none');
-		const row = table.insertRow();
-	   row.setAttribute('id', id++);
-		row.innerHTML =
-		`<td>${title.value}</td> 
-		<td>${description.value}</td>
-		<td class="text-center">
-			<input type="checkbox">
-		</td>
-		<td class="text-right">
-			<button class="btn btn-primary mb-1">
-				<i class="fa fa-pencil"></i>
-			</button>
-		</td>`;
-	   const removeBtn = document.createElement('button');
-	   removeBtn.classList.add('btn','btn-danger','mb-1','ml-1');
-	   removeBtn.innerHTML='<i class="fa fa-trash"></i>';
-	   removeBtn.onclick = function(e){
-		   removeTodo(row.getAttribute('id'));
-	   }
-	   row.children[3].appendChild(removeBtn);
-	}
-
-	btn.onClick = addTodo;
+  view.render();
 });
-
-function addTodo(){
-	const title = document.getElementById('title');
- 	const description = document.getElementById('description');
- 	const table = document.getElementById('table');
- 	const alert = document.getElementById('alert');
- 	const btn = document.getElementById('add');
-	 let id=1;
-	if(title.value === '' || description.value === ''){
-		alert.classList.remove('d-none');
-		alert.innerText= 'Titulo y descripcion requeridos';
-		return;
-	}
-   alert.classList.add('d-none');
-	const row = table.insertRow();
-	row.setAttribute('id', id++);
-	row.innerHTML =
-	`<td>${title.value}</td> 
-	<td>${description.value}</td>
-	<td class="text-center">
-		<input type="checkbox">
-	</td>
-	<td class="text-right">
-		<button class="btn btn-primary mb-1">
-			<i class="fa fa-pencil"></i>
-		</button>
-	</td>`;
-   const removeBtn = document.createElement('button');
-   removeBtn.classList.add('btn','btn-danger','mb-1');
-   removeBtn.innerHTML='<i class="fa fa-trash"></i>';
-   removeBtn.onclick = function(e){
-	removeTodo(row.getAttribute('id'));
-}
-   row.children[3].appendChild(removeBtn);
-}
-
-function removeTodo(id){
-	console.log(id);
-	document.getElementById(id).remove();
-}
